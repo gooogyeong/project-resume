ecd892271abfbef43696376b85c9e115eafb5ee7
Minkyung Lee <gooogyeong@gmail.com>
2022-10-07 01:34:32 +0900

    feature: add momenterie summary

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,42 @@ function Resume () {
         <Skill/>
       </div>
       <div className="resume__right">
+        <div>
+          <div className="title">About me</div>
+          <div>
+            <div style={{ fontSize: '1.6rem', 'margin': '1rem 0' }}>Hello, I am Minkyung Lee, applying for a Frontend Developer position at
+              Momenterie.<br/>
+              For following reasons, I find myself a fit for this position!
+            </div>
+            <div className="subtitle">Qualification<br/></div>
+            <ul>
+              <li>I have 2+ years of professional experience as a web frontend developer and mainly used Vue.js.<br/>
+                I migrated AngularJS app to Vue2 and now I'm developing new B2B app in Vue3.
+              </li>
+              <li>I have experience making web editor.<br/>
+                At work, I built an AngularJS web editor, which creates notice pages in HTML. Those notices were inserted as a
+                WebView into company's own app on a weekly basis.<br/>
+                During side project, I made another web editor in React. With this editor, authors of the project created posts
+                that include image, video, footnote, link, etc.
+              </li>
+            </ul>
+            <div className="subtitle">Culture</div>
+            <ul>
+              <li>I was happy to know that the head of engineering wrote the job description. I've always wanted to be
+                part
+                of a team with lead or senior frontend developers, whom I can ask if there's a better way to do things.
+              </li>
+            </ul>
+            <div className="subtitle">Product</div>
+            <ul>
+              <li>I was thrilled about the idea that if I join Momenterie, what I develop will be used by end
+                users.<br/>
+                I've always wanted to be challenged and learn from real user experiences and go beyond the scope of
+                admins, colleague developers and qa team.
+              </li>
+            </ul>
+          </div>
+        </div>
         <WorkExperience/>
       </div>
     </StyledResume>
@@ -22,7 +58,7 @@ function Resume () {
 const StyledResume = styled.div`
   display: flex;
   min-width: 100vw;
-  
+
   .title {
     font-weight: bold;
     text-transform: uppercase;
@@ -60,7 +96,7 @@ const StyledResume = styled.div`
       //text-decoration: none;
     }
   }
-  
+
 
   .dates-location-wrapper {
     display: flex;
