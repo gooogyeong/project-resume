7d0a5ced0a8defdfe0d71093bdf80a1ccaa0bdd7
Minkyung Lee <gooogyeong@gmail.com>
2022-10-08 20:49:21 +0900

    chore: removed summary

--- pages/index.js
+++ pages/index.js
@@ -15,42 +15,6 @@ function Resume () {
         <Skill/>
       </div>
       <div className="resume__right">
-        <div>
-          <div className="title">About me</div>
-          <div>
-            <div style={{ fontSize: '1.6rem', 'marginBottom': '2rem' }}>Hello, I am Minkyung, applying for a Frontend Developer position at
-              Momenterie.<br/>
-              For following reasons, I find myself a fit for this position!
-            </div>
-            <div className="subtitle">Qualification<br/></div>
-            <ul>
-              <li style={{ lineHeight: '2.4rem' }}>I have 2+ years of professional experience as a web frontend developer and wrote most of the codes in <Pill text='Vue.js'/>.<br/>
-                I migrated AngularJS app to Vue2 and now I&apos;m developing new B2B app in Vue3.
-              </li>
-              <li style={{ lineHeight: '2.4rem' }}>I have professional experience making web editors.<br/>
-                At work, I built an AngularJS web editor, with which admins create notice pages in HTML. Those notices are updated on a daily basis, inserted as a
-                WebView into company&apos;s own Android & iOS app.<br/>
-                During side project, I made another web editor in React. With this editor, authors of the project created posts
-                that include various media, such as image, video, footnote, link, etc. (<a href="https://modern-grotesque-times.web.app/write/guest"><IoMdLink style={{ color: '#CCCCCC' }}/> web editor for guest writer</a>)
-              </li>
-            </ul>
-            <div className="subtitle">Culture</div>
-            <ul>
-              <li style={{ lineHeight: '2.4rem' }}>I was happy to know that the head of engineering wrote the job description. I&apos;ve always wanted to be
-                part
-                of a team with lead or senior frontend developers, whom I can ask if there&apos;s a better way to do things.
-              </li>
-            </ul>
-            <div className="subtitle">Product</div>
-            <ul>
-              <li style={{ lineHeight: '2.4rem' }}>I was thrilled about the idea that if I join Momenterie, what I develop will be used by end
-                users.<br/>
-                I&apos;ve always wanted to be challenged and learn from real user experiences and go beyond the scope of
-                admins, colleague developers and qa team.
-              </li>
-            </ul>
-          </div>
-        </div>
         <WorkExperience/>
       </div>
     </StyledResume>
